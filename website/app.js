7db4fb5e6acf1c7c8f62a1908e87ebea9ce1d0c2
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-09-27 10:34:01 +0900

    Add config.js file to hide API key

--- website/app.js
+++ website/app.js
@@ -2,4 +2,6 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const API_KEY = config.API_KEY;
